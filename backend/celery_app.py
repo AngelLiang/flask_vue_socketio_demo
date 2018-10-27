3e9830f146a837fb11ad25ae64c5b29e7921376a
AngelLiang <pl01665077@163.com>
2018-10-27 23:26:34 +0800

    refactor(backend/): update celery_app

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -3,32 +3,29 @@
 celery worker -A celery_app.celery --loglevel=info
 """
 
+import os
+import sys
 import time
 from celery import Celery
 
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, curr_dir)
 
-def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
+# 加载.env环境变量
+from flask.cli import load_dotenv
+load_dotenv()
 
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
-
-from flask_app import app
-
-celery = make_celery(app)
+celery_conf = {
+    'CELERY_BROKER_URL': os.getenv('CELERY_BROKER_URL') or 'amqp://guest:guest@localhost:5672//',  # nopep8
+    'CELERY_RESULT_BACKEND': os.getenv('CELERY_RESULT_BACKEND') or 'amqp://guest:guest@localhost:5672//'  # nopep8
+}
+celery = Celery('celery_app', broker='amqp://guest:guest@localhost:5672//')
+celery.conf.update(celery_conf)
 
 
 @celery.task
 def background_task():
+    # 最好在函数里导入flask app，放在外面可能会导致socketio失效
     from flask_app import socketio, namespace
     socketio.emit(
         'data', {'data': 'Task starting...'},
